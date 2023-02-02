5d21ca760999ada07b28f46ce6b45f88dacf0ac7
Andrew Omodo <71156462+AndrewOmodo@users.noreply.github.com>
2023-02-02 22:22:45 +0300

    Tasks app added

---
+++ cs50django/tasks/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+tasks = ['foo', 'bar', 'buzz']
+# Create your views here.
+def index(request):
+    return render(request, "tasks/index.html", {
+        "tasks" : tasks,
+    }),
+
+def add(request):
+    return render(request, "tasks/add.html")
