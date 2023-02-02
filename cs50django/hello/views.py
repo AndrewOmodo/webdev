329014f3147669df4880e6ace51bccdf93f5c038
Andrew Omodo <71156462+AndrewOmodo@users.noreply.github.com>
2023-02-02 16:12:05 +0300

    Html beign viewed

--- cs50django/hello/views.py
+++ cs50django/hello/views.py
@@ -3,7 +3,8 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Hello, World!') #result from a HTTP request
+    #return HttpResponse('Hello, World!') #result from a HTTP request
+    return render(request, "hello/index.html")
 
 def andrew(request):
     return HttpResponse('Hello, Andrew!')
