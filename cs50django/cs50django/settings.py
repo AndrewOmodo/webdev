94537f10df835d23bf30a74aad7db931d380e23e
Andrew Omodo <71156462+AndrewOmodo@users.noreply.github.com>
2023-02-02 21:46:41 +0300

    Nothing much

--- cs50django/cs50django/settings.py
+++ cs50django/cs50django/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'newyear',
     'hello',
     'django.contrib.admin',
     'django.contrib.auth',
--- cs50django/cs50django/urls.py
+++ cs50django/cs50django/urls.py
@@ -19,5 +19,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('newyear', include('newyear.urls')),
     path('hello/', include('hello.urls')) # this includes all urls fromt the hello app
 ]
