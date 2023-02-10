1256850ea1c518444c3a3268312c30ce7c212688
Andrew Omodo <71156462+AndrewOmodo@users.noreply.github.com>
2023-02-10 13:26:01 +0300

    test.py updated to automate tests on the web app

---
+++ cs50code/test0.sh
@@ -0,0 +1,5 @@
+python -c "from test0 import test_prime; test_prime(1, False)"
+python -c "from test0 import test_prime; test_prime(2, True)"
+python -c "from test0 import test_prime; test_prime(8, False)"
+python -c "from test0 import test_prime; test_prime(25, False)"
+python -c "from test0 import test_prime; test_prime(28, False)"
